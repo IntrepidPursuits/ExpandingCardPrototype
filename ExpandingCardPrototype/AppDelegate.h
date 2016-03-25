519136eac7ef0deb302fe9454ddbadab9a606c8a
Brian Sachetta <sachetta@bc.edu>
2016-03-25 16:33:38 -0400

    Initial Commit

---
+++ ExpandingCardPrototype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpandingCardPrototype
+//
+//  Created by Brian Sachetta on 3/25/16.
+//  Copyright © 2016 IntrepidPursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExpandingCardPrototype/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExpandingCardPrototype
+//
+//  Created by Brian Sachetta on 3/25/16.
+//  Copyright © 2016 IntrepidPursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
